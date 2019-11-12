5c72a42ba25c199f6f20e0461751d32e74308dc0
jhulbert <joshua.hulbert@libertyits.com>
2019-11-12 11:51:08 -0500

    Return a duration when getting hangTime

--- bulk-fhir-api/src/main/java/gov/va/api/health/bulkfhir/api/internal/ClearHungRequest.java
+++ bulk-fhir-api/src/main/java/gov/va/api/health/bulkfhir/api/internal/ClearHungRequest.java
@@ -1,28 +1,29 @@
 package gov.va.api.health.bulkfhir.api.internal;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import javax.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Value;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-
 
 /**
-* The `/internal/publication/hung` endpoint will be automatically triggered using a
-* timer. This is the post body for the endpoint should someone wish to run it
-* manually (testing purposes, etc.).
-*/
+ * The `/internal/publication/hung` endpoint will be automatically triggered using a timer. This is
+ * the post body for the endpoint should someone wish to run it manually (testing purposes, etc.).
+ */
 @Value
 @Builder
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class ClearHungRequest {
 
-    /**
-    * We don't want to allow clearing of builds that have only just begun (0)
-    * therefor the min will be 0, max can be however long the person wants to
-    * all a build to wait for.
-    */
-    @NotNull
-    @Min(1)
-    Long hangTime;
+  @NotNull String hangTime;
+
+  /** Convert the input string to a Duration when getting hangTime. */
+  public Duration hangTime() {
+    String[] duration = hangTime.split(" ");
+    if (duration.length != 2) {
+      throw new RuntimeException("String hangTime is incorrectly formatted: " + hangTime);
+    }
+    return Duration.of(Long.parseLong(duration[0]), ChronoUnit.valueOf(duration[1].toUpperCase()));
+  }
 }
