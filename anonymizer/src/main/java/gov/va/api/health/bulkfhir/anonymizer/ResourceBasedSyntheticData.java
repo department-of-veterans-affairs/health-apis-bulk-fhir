995c868c402a8adad150d93d3576278950ab38c0
ilaflamme-va <43884028+ilaflamme-va@users.noreply.github.com>
2019-11-13 11:47:40 -0500

    Task/shank 109 anonymizer (#11)
    * WIP; anonymizer framework
    
    * WIP anonymizer
    
    * anonymizer complete
    
    * PR feedback
    
    * code complete
    
    * docs
    
    * formatting
    
    * proper datetime
    
    * fix tests

---
+++ anonymizer/src/main/java/gov/va/api/health/bulkfhir/anonymizer/ResourceBasedSyntheticData.java
@@ -0,0 +1,78 @@
+package gov.va.api.health.bulkfhir.anonymizer;
+
+import gov.va.api.health.dstu2.api.datatypes.HumanName;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+/** Class path resource based implementation of synthetic data. */
+@Slf4j
+@Builder
+public class ResourceBasedSyntheticData implements SyntheticData {
+
+  private final Names names;
+
+  /**
+   * We synthesize Dates and DateTimes by using: month = 1st month , day = first of the month This
+   * technique is a one way transformation and can't be reversed.
+   */
+  @Override
+  public String synthesizeDate(String rawDate) {
+    if (StringUtils.isBlank(rawDate)) {
+      return null;
+    }
+    LocalDate date;
+    try {
+      date = LocalDate.parse(rawDate);
+      date = date.withMonth(1);
+      date = date.withDayOfMonth(1);
+    } catch (DateTimeParseException e) {
+      log.info("Unable to parse the date [{}], using a default value.", rawDate);
+      date = LocalDate.of(2000, 1, 1);
+    }
+    return date.toString();
+  }
+
+  @Override
+  public String synthesizeDateTime(String rawDateTime) {
+    if (StringUtils.isBlank(rawDateTime)) {
+      return null;
+    }
+    OffsetDateTime date;
+    try {
+      date = OffsetDateTime.parse(rawDateTime);
+      date = date.withMonth(1);
+      date = date.withDayOfMonth(1);
+      date = date.withHour(12);
+      date = date.withMinute(34);
+      date = date.withSecond(56);
+      date = date.withOffsetSameInstant(ZoneOffset.UTC);
+
+    } catch (DateTimeParseException e) {
+      log.info("Unable to parse the dateTime [{}], using a defualt value.", rawDateTime);
+      date = OffsetDateTime.of(2000, 1, 1, 1, 1, 1, 0, ZoneOffset.UTC);
+    }
+    return date.toString();
+  }
+
+  /**
+   * We synthesize name using the ICN as a seed for selecting a name from a synthetic names
+   * resource. By using ICN as a seed, the name transformation is repeatable.
+   */
+  @Override
+  public List<HumanName> synthesizeName(long seed) {
+    var givenName = names.getName(seed);
+    var familyName = names.getName(seed + 1000);
+    return List.of(
+        HumanName.builder()
+            .family(List.of(familyName))
+            .given(List.of(givenName))
+            .text(givenName + " " + familyName)
+            .build());
+  }
+}
---
+++ anonymizer/src/test/java/gov/va/api/health/bulkfhir/anonymizer/patient/PatientAnonymizerTest.java
@@ -0,0 +1,68 @@
+package gov.va.api.health.bulkfhir.anonymizer.patient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import gov.va.api.health.bulkfhir.anonymizer.SyntheticData;
+import gov.va.api.health.bulkfhir.anonymizer.patient.AnonymizerPatientSamples.Anonymized;
+import gov.va.api.health.bulkfhir.anonymizer.patient.AnonymizerPatientSamples.Fhir;
+import gov.va.api.health.dstu2.api.datatypes.HumanName;
+import gov.va.api.health.dstu2.api.datatypes.HumanName.NameUse;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class PatientAnonymizerTest {
+
+  @Mock SyntheticData syntheticData;
+
+  PatientAnonymizer anonymizer() {
+    return PatientAnonymizer.builder().syntheticData(syntheticData).build();
+  }
+
+  /**
+   * Let's mock the synthesis process. We only want to test that the anonymizer is acting correctly
+   * on each patient field.
+   */
+  @Test
+  void apply() {
+    when(syntheticData.synthesizeName(Mockito.anyLong()))
+        .thenReturn(
+            List.of(
+                HumanName.builder()
+                    .use(NameUse.usual)
+                    .text("Mr. Sam Nobody")
+                    .family(List.of("Nobody"))
+                    .given(List.of("Sam"))
+                    .build()));
+    when(syntheticData.synthesizeDate("1970-11-14")).thenReturn("1970-10-01");
+    when(syntheticData.synthesizeDateTime("2001-03-03T15:08:09Z"))
+        .thenReturn("2001-02-01T00:00:00Z");
+    assertThat(anonymizer().apply(Fhir.create().patient()))
+        .isEqualTo(Anonymized.create().patient());
+  }
+
+  @Test
+  void icnBasedSeedReturnsHashWhenIcnIsGarbage() {
+    assertThat(anonymizer().icnBasedSeed("GARBAGE")).isEqualTo("GARBAGE".hashCode());
+  }
+
+  @Test
+  void icnBasedSeedReturnsLongWhenIcnIs10V6Format() {
+    Long expected = Long.valueOf("0123456789012345");
+    assertThat(anonymizer().icnBasedSeed("0123456789V012345")).isEqualTo(expected);
+  }
+
+  @Test
+  void sanitizeMultipleBirthBoolean() {
+    assertThat(anonymizer().sanitizeMultipleBirthBoolean(true, null)).isTrue();
+    assertThat(anonymizer().sanitizeMultipleBirthBoolean(false, null)).isFalse();
+    assertThat(anonymizer().sanitizeMultipleBirthBoolean(null, 2)).isTrue();
+    assertThat(anonymizer().sanitizeMultipleBirthBoolean(null, 0)).isFalse();
+    assertThat(anonymizer().sanitizeMultipleBirthBoolean(null, -1)).isFalse();
+  }
+}
