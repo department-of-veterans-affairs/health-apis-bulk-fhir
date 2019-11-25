002c94608a32f06b0c50ef1abb2951f231b85643
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2019-11-25 16:01:33 -0500

    Fix IT Tests (#22)
    * IT Test uses `client-key` header instead of `bulk-token`

--- bulk-fhir-tests/src/test/java/gov/va/api/health/bulkfhir/tests/ReadS3IT.java
+++ bulk-fhir-tests/src/test/java/gov/va/api/health/bulkfhir/tests/ReadS3IT.java
@@ -20,7 +20,7 @@ public class ReadS3IT {
     String path = apiPath() + "bulk/publication/index.json";
     log.info("Verify index.json is readable [{}]", path);
     TestClients.bulkFhir()
-        .get(ImmutableMap.of("bulk", System.getProperty("bulk-token", "default-value")), path)
+        .get(ImmutableMap.of("client-key", System.getProperty("bulk-token", "default-value")), path)
         .expect(200);
   }
 }
